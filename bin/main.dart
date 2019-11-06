e22dad9ab87c18754c5024ba15abaf84b0a4a3f2
Ashish <ashishteddy2@gmail.com>
2019-11-07 00:08:17 +0530

    done

--- bin/main.dart
+++ bin/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 // Challenge 1
 // Write test cases for the given challenges
 
@@ -7,7 +8,7 @@
 // that takes an integer, returning true if the integer is pandigital, and false otherwise.
 //  Examples
 //  isPandigital(98140723568910) ➞ true
-
+isPandigital (){}
 // Challenge 3
 // Area of Overlapping Rectangles
 // Create a function that returns the area of the overlap between two rectangles.
@@ -19,6 +20,23 @@
 //    [{ x: 2, y: 1 }, { x: 5, y: 5 }],
 //    [{ x: 3, y: 2 }, { x: 5, y: 7 }]
 //  ) ➞ 6
+
+
+
+
+int areaRectangle(List one, List two){
+  int area1 = (one[0] - two [0]).abs() *
+      (one[1] - two [1]).abs();
+  return area1;
+}
+int minPoint(int a, int b){
+  return a == b ? a : min(a, b);
+}
+
+int overlappingRectangle(List one, List two){
+  Point x;
+  return null;
+}
 
 main() {
 
