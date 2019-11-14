59b10d5cb7e6d68f87b4c460d281e5e4415f2071
Ashish <ashishteddy2@gmail.com>
2019-11-14 14:38:21 +0530

    Area calculation of overlapping rect

--- bin/Day_24_Morning_Challenge_test.dart
+++ bin/Day_24_Morning_Challenge_test.dart
@@ -7,11 +7,11 @@ void main() {
   });
 
   test('Rect test 1', (){
-    expect(overlappingRectangles([{ 'x': 5, 'y': 0 }, { 'x': 0, 'y': 10}],[{ 'x': 15, 'y': 15 }, { 'x': 20, 'y': 20 }]), true);
+    expect(overlappingRectangles([{ 'x': 5, 'y': 0 }, { 'x': 0, 'y': 10}],[{ 'x': 15, 'y': 15 }, { 'x': 20, 'y': 20 }]), 0);
 
   });
-  test('Rect test 1', (){
-    expect(overlappingRectangles([{ 'x': 2, 'y': 1 }, { 'x': 5, 'y': 5 }],[{ 'x': 3, 'y': 2 }, { 'x': 5, 'y': 7 }]), true);
+  test('Rect test 2', (){
+    expect(overlappingRectangles([{ 'x': 2, 'y': 1 }, { 'x': 5, 'y': 5 }],[{ 'x': 3, 'y': 2 }, { 'x': 5, 'y': 7 }]), 6);
 
   });
 }
--- bin/main.dart
+++ bin/main.dart
@@ -26,36 +26,52 @@ bool isPandigital(int num){
 //    [{ x: 3, y: 2 }, { x: 5, y: 7 }]
 //  ) ➞ 6
 
-int areaRectangle(List one, List two){
-  int area1 = (one[0] - two [0]).abs() *
-      (one[1] - two [1]).abs();
-  return area1;
+bool doOverlap(Map l1, Map r1, Map l2, Map r2) {
+  if (l1['x'] > r2['x'] || l2['x'] > r1['x']) return false; //If rect is on left side
+  return (l1['y'] < r2['y'] || l2['y'] < r1['y']); // If one rectangle is above other
 }
 
-int minPoint(int a, int b){
-  return a == b ? a : min(a, b);
+int overlappingRectangles(List one, List two){
+  print(overlappingArea(one[0], one[1], two[0], two[1]))  ;
+  //return(doOverlap(one[0], one[1], two[0], two[1]));
+  return (overlappingArea(one[0], one[1], two[0], two[1]));
 }
 
-bool doOverlap(Map l1, Map r1, Map l2, Map r2)
-{
+int overlappingArea(Map l1, Map r1, Map l2, Map r2) {
+  // Area of 1st Rectangle
+  int area1 = (l1['x'] - r1['x']).abs() *
+      (l1['y'] - r1['y']).abs();
 
-  //if (l1['x'] > r2['x'] || l2['x'] > r1['x'])  return false; //If rect is on left side
-  //if (l1['y'] < r2['y'] || l2['y'] < r1['y'])  return false; //If they are inside each other
+  // Area of 2nd Rectangle
+  int area2 = (l2['x'] - r2['x']).abs() *
+      (l2['y'] - r2['y']).abs();
 
-  return ((l1['x'] > r2['x'] || l2['x'] > r1['x']) || (l1['y'] < r2['y'] || l2['y'] < r1['y']));
-}
+  List <int> rightx= [r1['x'], r2['x']];
+  List <int> leftx= [l1['x'], l2['x']];
+  List <int> righty= [r1['y'], r2['y']];
+  List <int> lefty= [l1['y'], l2['y']];
 
+  int areaI = (rightx.reduce(min) - leftx.reduce(min)) *
+              (righty.reduce(min) - lefty.reduce(min));
 
-bool overlappingRectangles(List one, List two){
-    return(doOverlap(one[0], one[1], two[0], two[1]));
+  if (areaI < 0){
+    return 0;
+  }
+  else {
+    return (areaI / 2).round();
+  }
 }
 
 main() {
   print(isPandigital(98140723568910));
-  print(
-      overlappingRectangles(
+  print(overlappingRectangles(
     [{ 'x': 2, 'y': 1 }, { 'x': 5, 'y': 5 }],
     [{ 'x': 3, 'y': 2 }, { 'x': 5, 'y': 7 }]
+    )
+  );
+  print(overlappingRectangles(
+      [{ 'x': 2, 'y': 1 }, { 'x': 5, 'y': 5 }],
+      [{ 'x': 3, 'y': 2 }, { 'x': 5, 'y': 7 }]
   )
   );
   
