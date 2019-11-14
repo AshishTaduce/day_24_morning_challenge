b7d7776fe6c182b83d3026e21ade852df7a31c48
Ashish <ashishteddy2@gmail.com>
2019-11-14 13:46:01 +0530

    done 1.0

--- bin/main.dart
+++ bin/main.dart
@@ -1,4 +1,5 @@
 import 'dart:math';
+
 // Challenge 1
 // Write test cases for the given challenges
 
@@ -8,10 +9,11 @@ import 'dart:math';
 // that takes an integer, returning true if the integer is pandigital, and false otherwise.
 //  Examples
 //  isPandigital(98140723568910) ➞ true
+
 bool isPandigital(int num){
-  List numbers = num.toString().split('').toList();
-  return numbers.toSet().length == 10;
+  return(num.toString().split('').toSet().toList().length == 10);
 }
+
 // Challenge 3
 // Area of Overlapping Rectangles
 // Create a function that returns the area of the overlap between two rectangles.
@@ -23,21 +25,38 @@ bool isPandigital(int num){
 //    [{ x: 2, y: 1 }, { x: 5, y: 5 }],
 //    [{ x: 3, y: 2 }, { x: 5, y: 7 }]
 //  ) ➞ 6
+
 int areaRectangle(List one, List two){
   int area1 = (one[0] - two [0]).abs() *
       (one[1] - two [1]).abs();
   return area1;
 }
+
 int minPoint(int a, int b){
   return a == b ? a : min(a, b);
 }
 
-int overlappingRectangle(List one, List two){
-  Point x;
-  return null;
+bool doOverlap(Map l1, Map r1, Map l2, Map r2)
+{
+
+  //if (l1['x'] > r2['x'] || l2['x'] > r1['x'])  return false; //If rect is on left side
+  //if (l1['y'] < r2['y'] || l2['y'] < r1['y'])  return false; //If they are inside each other
+
+  return ((l1['x'] > r2['x'] || l2['x'] > r1['x']) || (l1['y'] < r2['y'] || l2['y'] < r1['y']));
+}
+
+
+bool overlappingRectangles(List one, List two){
+    return(doOverlap(one[0], one[1], two[0], two[1]));
 }
 
 main() {
   print(isPandigital(98140723568910));
-
+  print(
+      overlappingRectangles(
+    [{ 'x': 2, 'y': 1 }, { 'x': 5, 'y': 5 }],
+    [{ 'x': 3, 'y': 2 }, { 'x': 5, 'y': 7 }]
+  )
+  );
+  
 }
--- test/Day_24_Morning_Challenge_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:Day_24_Morning_Challenge/Day_24_Morning_Challenge.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('isPandigital', () {
-    expect(calculate(), 42);
-  });
-}
